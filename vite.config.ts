c46f68d3a16ee6d4471cfde20f3a09fd8e6f328d
Cedric Lee <cedmanila@gmail.com>
2026-04-13 22:29:02 +0800

    Performance fixes

--- vite.config.ts
+++ vite.config.ts
@@ -6,10 +6,24 @@ import react from '@vitejs/plugin-react'
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url))
 const disbRoot = path.resolve(__dirname, 'src/prototypes/disb')
+const ENTRY_CSS_RE = /<link rel="stylesheet"([^>]*href="\/assets\/index-[^"]+\.css"[^>]*)>/i
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    {
+      name: 'async-entry-stylesheet',
+      transformIndexHtml(html) {
+        return html.replace(
+          ENTRY_CSS_RE,
+          (_match, attrs) =>
+            `<link rel="preload" as="style"${attrs} onload="this.onload=null;this.rel='stylesheet'"><noscript><link rel="stylesheet"${attrs}></noscript>`,
+        )
+      },
+    },
+  ],
   resolve: {
     alias: [{ find: /^@disb\/(.+)$/, replacement: `${disbRoot}/$1` }],
   },
